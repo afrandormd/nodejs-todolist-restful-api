dc64e18609a4513268fa95889f46059b029cd7ea
afrandormd <afrandormd@gmail.com>
2025-08-05 13:30:40 +0700

    feat: buat service POST API Todolist

--- todolist-service.mjs
+++ todolist-service.mjs
@@ -2,6 +2,7 @@ export class TodolistService {
   // Tempat menyimpan data Todolist
   todolist = ["Belajar", "Olahraga", "Masak"];
 
+  // Bentuk response
   getJsonTodolist() {
     return JSON.stringify({
       code: 200,
@@ -19,5 +20,20 @@ export class TodolistService {
   getTodoList(request, response) {
     response.write(this.getJsonTodolist());
     response.end();
+  }
+
+  // Fungsi untuk POST data Todolist
+  createTodo(request, response) {
+    request.addListener("data", (data) => {
+      // Parsing data buffer -> string
+      const body = JSON.parse(data.toString());
+
+      // Masukkan data ke array todolist
+      this.todolist.push(body.todo);
+
+      // Kembalikan response
+      response.write(this.getJsonTodolist());
+      response.end();
+    });
   }
 }
