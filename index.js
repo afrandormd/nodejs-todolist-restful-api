d56f5b79ef64787f45c9c5289966202434b577a2
afrandormd <afrandormd@gmail.com>
2025-08-05 15:08:06 +0700

    feat: buat route index

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const swaggerDocument = JSON.parse(raw);
 // Serve Swagger UI files
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Routes
+app.get("/", (req, res) => {
+  res.send("NodeJS Todolist RESTful API");
+});
+
 app.listen(PORT, () => {
   console.log(`Swagger UI available at http://localhost:${PORT}/api-docs`);
 });
